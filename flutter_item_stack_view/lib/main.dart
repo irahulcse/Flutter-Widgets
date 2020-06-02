f5aedacad8d27fba75b0efa3534ed81220e51c42
Rahul Chandra <1rahulchandra1@gmail.com>
2020-06-02 22:13:09 +0530

    Automatic Commit 2020-06-02-22:13:09

--- flutter_item_stack_view/lib/main.dart
+++ flutter_item_stack_view/lib/main.dart
@@ -1,3 +1,356 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'const.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   final CategoriesScroller categoriesScroller = CategoriesScroller();
+//   ScrollController controller = ScrollController();
+//   bool closeTopContainer = false;
+
+//   double topContainer = 0;
+
+//   List<Widget> itemsData = [];
+
+//   void getPostsData() {
+//     List<dynamic> responseList = FOOD_DATA;
+//     List<Widget> listItems = [];
+//     responseList.forEach((post) {
+//       listItems.add(Container(
+//           height: 150,
+//           margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+//           decoration: BoxDecoration(
+//               borderRadius: BorderRadius.all(Radius.circular(20.0)),
+//               color: Colors.white,
+//               boxShadow: [
+//                 BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0),
+//               ]),
+//           child: Padding(
+//             padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: <Widget>[
+//                 Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: <Widget>[
+//                     Text(
+//                       post["name"],
+//                       style: const TextStyle(
+//                           fontSize: 28, fontWeight: FontWeight.bold),
+//                     ),
+//                     Text(
+//                       post["brand"],
+//                       style: const TextStyle(fontSize: 17, color: Colors.grey),
+//                     ),
+//                     SizedBox(
+//                       height: 10,
+//                     ),
+//                     Text(
+//                       "\$ ${post["price"]}",
+//                       style: const TextStyle(
+//                           fontSize: 25,
+//                           color: Colors.black,
+//                           fontWeight: FontWeight.bold),
+//                     )
+//                   ],
+//                 ),
+//                 Image.asset(
+//                   "assets/images/${post["image"]}",
+//                   height: double.infinity,
+//                 )
+//               ],
+//             ),
+//           )));
+//     });
+//     setState(() {
+//       itemsData = listItems;
+//     });
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     getPostsData();
+//     controller.addListener(() {
+//       setState(() {
+//         closeTopContainer = controller.offset > 50;
+//       });
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final Size size = MediaQuery.of(context).size;
+//     // final double categoryHeight = size.height * 0.30;
+//     final double categoryHeight = size.height * 0.30;
+//     return SafeArea(
+//       child: Scaffold(
+//         backgroundColor: Colors.white,
+//         appBar: AppBar(
+//           elevation: 0,
+//           backgroundColor: Colors.white,
+//           leading: Icon(
+//             Icons.menu,
+//             color: Colors.black,
+//           ),
+//           actions: <Widget>[
+//             IconButton(
+//               icon: Icon(Icons.search, color: Colors.black),
+//               onPressed: () {},
+//             ),
+//             IconButton(
+//               icon: Icon(Icons.person, color: Colors.black),
+//               onPressed: () {},
+//             )
+//           ],
+//         ),
+//         body: Container(
+//           height: size.height,
+//           child: Column(
+//             children: <Widget>[
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                 children: <Widget>[
+//                   Text(
+//                     "Loyality Cards",
+//                     style: TextStyle(
+//                         color: Colors.grey,
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 20),
+//                   ),
+//                   Text(
+//                     "Menu",
+//                     style: TextStyle(
+//                         color: Colors.black,
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 20),
+//                   ),
+//                 ],
+//               ),
+//               const SizedBox(
+//                 height: 10,
+//               ),
+//               AnimatedOpacity(
+//                 duration: Duration(milliseconds: 200),
+//                 opacity: closeTopContainer ? 0 : 1,
+//                 child: AnimatedContainer(
+//                   child: categoriesScroller,
+//                   duration: Duration(
+//                     milliseconds: 200,
+//                   ),
+//                   alignment: Alignment.topCenter,
+//                   width: size.width,
+//                   height: closeTopContainer ? 0 : categoryHeight,
+//                 ),
+//               ),
+//               // Expanded(
+//               //   child: ListView.builder(
+//               //     controller: controller,
+//               //     itemCount: itemsData.length,
+//               //     physics: BouncingScrollPhysics(),
+//               //     itemBuilder: (context, index) {
+//               //       double scale = 1.0;
+//               //       if (topContainer > 0.5) {
+//               //         scale = index + 0.5 - topContainer;
+//               //         if (scale < 0) {
+//               //           scale = 0;
+//               //         } else if (scale > 1) {
+//               //           scale = 1;
+//               //         }
+//               //       }
+//               //       return Opacity(
+//               //         opacity: scale,
+//               //         child: Transform(
+//               //           transform: Matrix4.identity()..scale(scale, scale),
+//               //           alignment: Alignment.bottomCenter,
+//               //           child: Align(
+//               //               heightFactor: 0.7,
+//               //               alignment: Alignment.topCenter,
+//               //               child: itemsData[index]),
+//               //         ),
+//               //       );
+//               //     },
+//               //   ),
+//               // ),
+//               Expanded(
+//                 child: ListView.builder(
+//                   controller: controller,
+//                   itemCount: itemsData.length,
+//                   itemBuilder: (BuildContext context, int index) {
+//                     double scale = 1.0;
+//                     if (topContainer > 0.5) {
+//                       scale = index + 0.5 - topContainer;
+//                       if (scale < 0) {
+//                         scale = 0;
+//                       } else if (scale > 1) {
+//                         scale = 1;
+//                       }
+//                     }
+
+//                     return Opacity(
+//                       opacity: scale,
+//                       child: Transform(
+//                         transform: Matrix4.identity()..scale(scale, scale),
+//                         alignment: Alignment.bottomCenter,
+//                         child: Align(
+//                           child: itemsData[index],
+//                           heightFactor: 0.7,
+//                           alignment: Alignment.topCenter,
+//                         ),
+//                       ),
+//                     );
+//                   },
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class CategoriesScroller extends StatelessWidget {
+//   const CategoriesScroller();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final double categoryHeight =
+//         MediaQuery.of(context).size.height * 0.30 - 50;
+
+//     return SingleChildScrollView(
+//       physics: BouncingScrollPhysics(),
+//       scrollDirection: Axis.horizontal,
+//       child: Container(
+//         margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+//         child: FittedBox(
+//           alignment: Alignment.topCenter,
+//           fit: BoxFit.fill,
+//           child: Row(
+//             children: <Widget>[
+//               Container(
+//                 width: 150,
+//                 margin: EdgeInsets.only(right: 20),
+//                 height: categoryHeight,
+//                 decoration: BoxDecoration(
+//                     color: Colors.orange.shade400,
+//                     borderRadius: BorderRadius.all(Radius.circular(20.0))),
+//                 child: Padding(
+//                   padding: const EdgeInsets.all(12.0),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: <Widget>[
+//                       Text(
+//                         "Most\nFavorites",
+//                         style: TextStyle(
+//                             fontSize: 25,
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.bold),
+//                       ),
+//                       SizedBox(
+//                         height: 10,
+//                       ),
+//                       Text(
+//                         "20 Items",
+//                         style: TextStyle(fontSize: 16, color: Colors.white),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//               Container(
+//                 width: 150,
+//                 margin: EdgeInsets.only(right: 20),
+//                 height: categoryHeight,
+//                 decoration: BoxDecoration(
+//                     color: Colors.blue.shade400,
+//                     borderRadius: BorderRadius.all(Radius.circular(20.0))),
+//                 child: Container(
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(12.0),
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: <Widget>[
+//                         Text(
+//                           "Newest",
+//                           style: TextStyle(
+//                               fontSize: 25,
+//                               color: Colors.white,
+//                               fontWeight: FontWeight.bold),
+//                         ),
+//                         SizedBox(
+//                           height: 10,
+//                         ),
+//                         Text(
+//                           "20 Items",
+//                           style: TextStyle(fontSize: 16, color: Colors.white),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               Container(
+//                 width: 150,
+//                 margin: EdgeInsets.only(right: 20),
+//                 height: categoryHeight,
+//                 decoration: BoxDecoration(
+//                     color: Colors.lightBlueAccent.shade400,
+//                     borderRadius: BorderRadius.all(Radius.circular(20.0))),
+//                 child: Padding(
+//                   padding: const EdgeInsets.all(12.0),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: <Widget>[
+//                       Text(
+//                         "Super\nSaving",
+//                         style: TextStyle(
+//                             fontSize: 25,
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.bold),
+//                       ),
+//                       SizedBox(
+//                         height: 10,
+//                       ),
+//                       Text(
+//                         "20 Items",
+//                         style: TextStyle(fontSize: 16, color: Colors.white),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'const.dart';
@@ -30,7 +383,6 @@ class _MyHomePageState extends State<MyHomePage> {
   final CategoriesScroller categoriesScroller = CategoriesScroller();
   ScrollController controller = ScrollController();
   bool closeTopContainer = false;
-
   double topContainer = 0;
 
   List<Widget> itemsData = [];
@@ -59,7 +411,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     Text(
                       post["name"],
                       style: const TextStyle(
-                          fontSize: 28, fontWeight: FontWeight.bold),
+                        color: Colors.purple,
+                          fontSize: 28, fontWeight: FontWeight.w200),
                     ),
                     Text(
                       post["brand"],
@@ -95,7 +448,10 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     getPostsData();
     controller.addListener(() {
+      double value = controller.offset / 119;
+
       setState(() {
+        topContainer = value;
         closeTopContainer = controller.offset > 50;
       });
     });
@@ -104,7 +460,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    // final double categoryHeight = size.height * 0.30;
     final double categoryHeight = size.height * 0.30;
     return SafeArea(
       child: Scaffold(
@@ -154,56 +509,42 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 10,
               ),
               AnimatedOpacity(
-                duration: Duration(milliseconds: 200),
+                duration: const Duration(milliseconds: 200),
                 opacity: closeTopContainer ? 0 : 1,
                 child: AnimatedContainer(
-                  child: categoriesScroller,
-                  duration: Duration(
-                    milliseconds: 200,
-                  ),
-                  alignment: Alignment.topCenter,
-                  width: size.width,
-                  height: closeTopContainer ? 0 : categoryHeight,
-                ),
+                    duration: const Duration(milliseconds: 200),
+                    width: size.width,
+                    alignment: Alignment.topCenter,
+                    height: closeTopContainer ? 0 : categoryHeight,
+                    child: categoriesScroller),
               ),
-              // Expanded(
-              //   child: ListView.builder(
-              //     controller: controller,
-              //     itemCount: itemsData.length,
-              //     physics: BouncingScrollPhysics(),
-              //     itemBuilder: (context, index) {
-              //       double scale = 1.0;
-              //       if (topContainer > 0.5) {
-              //         scale = index + 0.5 - topContainer;
-              //         if (scale < 0) {
-              //           scale = 0;
-              //         } else if (scale > 1) {
-              //           scale = 1;
-              //         }
-              //       }
-              //       return Opacity(
-              //         opacity: scale,
-              //         child: Transform(
-              //           transform: Matrix4.identity()..scale(scale, scale),
-              //           alignment: Alignment.bottomCenter,
-              //           child: Align(
-              //               heightFactor: 0.7,
-              //               alignment: Alignment.topCenter,
-              //               child: itemsData[index]),
-              //         ),
-              //       );
-              //     },
-              //   ),
-              // ),
               Expanded(
-                child: ListView.builder(
-                  controller: controller,
-                  itemCount: itemsData.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return itemsData[index];
-                  },
-                ),
-              ),
+                  child: ListView.builder(
+                      controller: controller,
+                      itemCount: itemsData.length,
+                      physics: BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        double scale = 1.0;
+                        if (topContainer > 0.5) {
+                          scale = index + 0.5 - topContainer;
+                          if (scale < 0) {
+                            scale = 0;
+                          } else if (scale > 1) {
+                            scale = 1;
+                          }
+                        }
+                        return Opacity(
+                          opacity: scale,
+                          child: Transform(
+                            transform: Matrix4.identity()..scale(scale, scale),
+                            alignment: Alignment.bottomCenter,
+                            child: Align(
+                                heightFactor: 0.7,
+                                alignment: Alignment.topCenter,
+                                child: itemsData[index]),
+                          ),
+                        );
+                      })),
             ],
           ),
         ),
@@ -225,8 +566,8 @@ class CategoriesScroller extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
         child: FittedBox(
-          alignment: Alignment.topCenter,
           fit: BoxFit.fill,
+          alignment: Alignment.topCenter,
           child: Row(
             children: <Widget>[
               Container(
